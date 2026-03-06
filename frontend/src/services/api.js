480aeb0dc6b81c86bc5057c6fc1f2a738dd1df97
Amolika28-Kale <amolikakale234@gmail.com>
2026-03-06 16:39:23 +0530

    api chnged

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -2,7 +2,9 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:5000/api"
+  // baseURL: "http://localhost:5000/api"
+    baseURL: "https://farmtrack-animal-management-system.onrender.com/api"
+
 });
 
 // Add token to requests
